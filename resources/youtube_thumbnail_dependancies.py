0f5b1fbd0e9c77ac841246cb7b8413448c385c94
nednoodlehead <102321690+nednoodlehead@users.noreply.github.com>
2022-06-15 15:54:57 -0700

    Deleted unnecessary folder

--- resources/youtube_thumbnail_dependancies.py
+++
@@ -1,4 +0,0 @@
-file_loc0 = "F:/Files at random/Downloaded thumbnails"
-file_loc1 = "F:/Download Folder"
-file_loc2 = "F:/Files at random/Downloaded thumbnails"
-file_loc3 = "F:/Files at random/Downloaded thumbnails"
